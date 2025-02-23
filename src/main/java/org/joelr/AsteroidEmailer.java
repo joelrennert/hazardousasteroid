b7f81fcd1c4b16bceec7d898cd5947dacda6d958
Joel R <129562199+joelrennert@users.noreply.github.com>
2025-02-23 12:26:09 -0500

    refactored main class into more focused methods

--- src/main/java/org/joelr/AsteroidEmailer.java
+++ src/main/java/org/joelr/AsteroidEmailer.java
@@ -39,29 +39,45 @@ public class AsteroidEmailer {
         AsteroidInfoBuilder infoBuilder = new AsteroidInfoBuilder();
 
         try {
-            List<NasaNeoResponse.NearEarthObject> nearEarthObjects = apiClient.fetchNearEarthObjects(apiKey);
+            List<NasaNeoResponse.NearEarthObject> nearEarthObjects = fetchNearEarthObjects(apiClient, apiKey);
+            List<NasaNeoResponse.NearEarthObject> hazardousObjects = filterHazardousAsteroids(nearEarthObjects);
+            String emailContent = buildEmailContent(hazardousObjects, infoBuilder);
 
-            List<NasaNeoResponse.NearEarthObject> hazardousObjects = new ArrayList<>();
-            for (NasaNeoResponse.NearEarthObject neo : nearEarthObjects) {
-                if (neo.isPotentiallyHazardousAsteroid()) {
-                    hazardousObjects.add(neo);
-                }
-            }
-
-            StringBuilder emailContent = new StringBuilder();
-            for (NasaNeoResponse.NearEarthObject neo : hazardousObjects) {
-                String neoInfo = infoBuilder.buildAsteroidInfo(neo);
-                emailContent.append(neoInfo).append("<br/><br/>");
-            }
-
-            if (emailContent.length() > 0) {
-                emailService.sendHazardousAsteroidEmail(emailContent.toString());
-            } else {
-                System.out.println("No hazardous asteroids found. No email sent.");
-            }
+            sendEmailIfNeeded(emailContent, emailService);
 
         } catch (IOException e) {
             System.err.println("Error fetching data from NASA NEO API: " + e.getMessage());
+        }
+    }
+
+    private static List<NasaNeoResponse.NearEarthObject> fetchNearEarthObjects(NasaApiClient apiClient, String apiKey) throws IOException {
+        return apiClient.fetchNearEarthObjects(apiKey);
+    }
+
+    private static List<NasaNeoResponse.NearEarthObject> filterHazardousAsteroids(List<NasaNeoResponse.NearEarthObject> nearEarthObjects) {
+        List<NasaNeoResponse.NearEarthObject> hazardousObjects = new ArrayList<>();
+        for (NasaNeoResponse.NearEarthObject neo : nearEarthObjects) {
+            if (neo.isPotentiallyHazardousAsteroid()) {
+                hazardousObjects.add(neo);
+            }
+        }
+        return hazardousObjects;
+    }
+
+    private static String buildEmailContent(List<NasaNeoResponse.NearEarthObject> hazardousObjects, AsteroidInfoBuilder infoBuilder) {
+        StringBuilder emailContent = new StringBuilder();
+        for (NasaNeoResponse.NearEarthObject neo : hazardousObjects) {
+            String neoInfo = infoBuilder.buildAsteroidInfo(neo);
+            emailContent.append(neoInfo).append("<br/><br/>");
+        }
+        return emailContent.toString();
+    }
+
+    private static void sendEmailIfNeeded(String emailContent, EmailService emailService) {
+        if (!emailContent.isEmpty()) {
+            emailService.sendHazardousAsteroidEmail(emailContent);
+        } else {
+            System.out.println("No hazardous asteroids found. No email sent.");
         }
     }
 }
